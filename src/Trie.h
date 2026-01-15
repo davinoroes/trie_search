58aa6a26b34508caf708fb43374ebc49f7f6dff5
davinoroes <davinoroes1918@gmail.com>
2026-01-15 18:20:38 -0300

    Criação da class Trie que contém as declarações de funções

--- src/Trie.h
+++ src/Trie.h
@@ -9,7 +9,33 @@ struct TrieNode{
     int count; //numero de ocorrencias da palavra no arquivo
 };
 
+class Trie {
+public:
+    Trie(); //construtor
 
+    ~Trie(); //destrutor
+
+    void insert(const string &word); //declaracao da funcao de insercao de palavra
+
+    bool search(const string &word); //declaracao da busca pela palavra(vai retornar true se a palavrar tiver no arquivo)
+
+    int get_count(const string &word); //aqui vai ser a contagem de quantas vezes a palavra aparece (achei um projeto muito simples apenas dizer se a palavra tá la o não)
+
+
+private:
+    TrieNode *root;
+
+    //criacao e iniacao de um nó
+    TrieNode* create_node();
+
+    void freenode(TrieNode *node); 
+    /*
+    vou fazer uma funçao de liberar memoria pois por mais 
+    q seja uma implemetção otimizada no ponto de vista de 
+    complexidade, no ponto de vista de memoria é ruim
+    */
+
+};
 
 #endif
 
